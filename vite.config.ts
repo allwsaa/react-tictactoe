7e009a752796756b1711229aee3b87264d381536
allwsaa <bekbosynova.alima@inbox.ru>
2024-10-13 16:18:41 +0500

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+
+import react from '@vitejs/plugin-react';
+import legacy from '@vitejs/plugin-legacy';
+
+import targets from 'browserslist-config-google';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/',
+  plugins: [react(), legacy({ targets })],
+});
